2424983499908558af56bea42c808616a9675964
macchiagabriele13 <macchia_gabriele@libero.it>
2022-11-08 16:06:33 +0100

    Commit - Creo ciclo per 10 email

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,25 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      my_email: null
+      my_email:[]
     }
   },
+  methods:{
+    getEmails(){ 
+        for (let index = 0; index < 10; index++) {
+            
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data);
+                this.my_email.push(response.data.response)
+            } )
+            
+        }
+    }
+
+  },
   mounted(){
-    axios
-    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((response) => {
-        console.log(response.data);
-        this.my_email = response.data.response
-    } )
+    this.getEmails()
   }
 }).mount('#app')
